805e58509bbca814e6a811444efbead3f6e3337c
kafkaphoenix <jaguilerapuerta@gmail.com>
2018-12-25 12:50:04 +0100

    Smart Pointers Case Study
    Exercises from Expressive code with C++ smart pointers's book by fluentcpp

--- smartpointers_case_study/main.cpp
+++ smartpointers_case_study/main.cpp
@@ -1,1 +1,19 @@
+#include <iostream>
+using std::cout;
+using std::endl;
+using std::cin;
+#include <string>
+using std::string;
+#include <vector>
+using std::vector;
+#include <algorithm>
+using std::find;
+#include <list>
+using std::list;
 
+int main()
+{
+
+
+    return 0;
+}
